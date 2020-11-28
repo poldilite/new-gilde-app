119ade653489cab4e19478877a4addf1c7a25d61
Martin Olligschlaeger <martin.olligschlaeger@gmail.com>
2020-11-28 11:35:26 +0100

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './comps/main/main.component';
+import { TimetableComponent } from './comps/timetable/timetable.component';
+import { HeaderComponent } from './comps/header/header.component';
+import { FooterComponent } from './comps/footer/footer.component';
+import { HomeComponent } from './comps/home/home.component';
+import { ImpressumComponent } from './comps/impressum/impressum.component';
+import { VideoComponent } from './comps/video/video.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TimetableComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ImpressumComponent,
+    VideoComponent
   ],
   imports: [
     BrowserModule,
